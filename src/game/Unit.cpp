d8d57ae22bfd148cdf5604c5238ce47b2592b4ff
SilverIce <slifeleaf@gmail.com>
2010-07-17 03:26:36 +0400

    Broadcast packets only if worldobject is in world
    There is no sense to do that for objects that not in world - those objects are unknown for clients
    and their packets will be ignored
    
    (based on SilverIce's repo commit 6305402)
    
    Signed-off-by: VladimirMangos <vladimir@getmangos.com>

--- src/game/Unit.cpp
+++ src/game/Unit.cpp
@@ -8984,7 +8984,7 @@ void Unit::SetPower(Powers power, uint32 val)
     data << GetPackGUID();
     data << uint8(power);
     data << uint32(val);
-    SendMessageToSet(&data, GetTypeId() == TYPEID_PLAYER ? true : false);
+    SendMessageToSet(&data, true);
 
     // group update
     if(GetTypeId() == TYPEID_PLAYER)
